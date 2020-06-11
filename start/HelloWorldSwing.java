1b5a5ea87b2a34bf4049d2a4c9206cdf314acf3a
Lemmiwinkz <66788262+Lemmiwinkz@users.noreply.github.com>
2020-06-11 19:43:03 +0200

    1. Günni meeting

--- start/HelloWorldSwing.java
+++ start/HelloWorldSwing.java
@@ -21,7 +21,7 @@ public class HelloWorldSwing {
         frame.pack();
         frame.setVisible(true);
     }
- 
+    		/** HEHO GÜNNI */
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
